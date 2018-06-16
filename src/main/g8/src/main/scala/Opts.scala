8c76af64474f2ffe617dc54860fb374ae207c360
Jeffrey Massung <massung@gmail.com>
2018-06-16 18:07:48 -0500

    Fixed skeleton compile error.

--- src/main/g8/src/main/scala/Opts.scala
+++ src/main/g8/src/main/scala/Opts.scala
@@ -4,7 +4,7 @@ import org.rogach.scallop._
  * Command line argument parsing.
  */
 class Opts(args: Array[String]) extends ScallopConf(args) {
-  val msg = opt[String]
+  val message = opt[String]()
 
   // parse the arguments
   verify
