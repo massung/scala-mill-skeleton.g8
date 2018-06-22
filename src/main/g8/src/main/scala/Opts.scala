b7d124541318b5f93025e7323e57d78697959e87
Jeffrey Massung <massung@gmail.com>
2018-06-22 08:21:43 -0400

    Removed sample JSON load from Opts.

--- src/main/g8/src/main/scala/Opts.scala
+++ src/main/g8/src/main/scala/Opts.scala
@@ -1,12 +1,4 @@
-import io.circe._
-import io.circe.generic.auto._
-import io.circe.parser._
-import io.circe.syntax._
-
 import org.rogach.scallop._
-
-import scala.io.Source
-import scala.util.Try
 
 /**
  * Command line argument parsing.
@@ -16,19 +8,4 @@ class Opts(args: Array[String]) extends ScallopConf(args) {
 
   // parse the arguments
   verify
-}
-
-/**
- * Companion object for parsing JSON config files.
- */
-object Opts {
-
-  /**
-   * Read a JSON file, parse, and decode it.
-   */
-  def loadOptsFile[T](path: String): Try[Json] = {
-    Try(Source.fromFile(path).mkString) flatMap {
-      json => parse(json).flatMap(_.as[T]).toTry
-    }
-  }
 }
